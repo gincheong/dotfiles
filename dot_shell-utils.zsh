ed11b5236dcaa9b431f63d1229fb5ca42b33f551
Evan <evan@shoplive.cloud>
2026-06-10 13:25:47 +0900

    feat: alias name edited groot -> cdgt

--- dot_shell-utils.zsh
+++ dot_shell-utils.zsh
@@ -4,7 +4,7 @@
 # .zshrc에서 다음과 같이 source 하여 사용
 # [ -f "$HOME/.shell-utils.zsh" ] && source "$HOME/.shell-utils.zsh"
 
-alias groot='cd "$(git rev-parse --show-toplevel)"'
+alias cdgt='cd "$(git rev-parse --show-toplevel)"'
 
 alias cddown='cd ~/Downloads'
 

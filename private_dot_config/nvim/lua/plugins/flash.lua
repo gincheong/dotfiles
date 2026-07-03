4537b5a973d4578bc88a3cb66eea5721bc390487
Evan <evan@shoplive.cloud>
2026-07-03 11:13:00 +0900

    nvim flash plugin

---
+++ private_dot_config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      search = {
+        enabled = true
+      },
+    }
+  },
+  keys = {
+    { "s", mode = { "n" }, function() require("flash").jump({
+      jump = { register = true, history = true },
+    }) end, desc = "Flash" },
+    -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+}
